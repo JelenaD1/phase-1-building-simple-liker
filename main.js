d615f3409a9aa6de9409e0ce77e99363aedd8226
Jelena <jelena.durovic@powerhrg.com>
2021-06-03 21:53:47 -0400

    got it

--- main.js
+++ main.js
@@ -2,9 +2,36 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
+const heart = document.querySelectorAll(".like-glyph")
+heart.forEach( (element) => {
+  
+  element.addEventListener("click", () => {
 
+    mimicServerCall()
+    .then(() => {
+
+      if(element.innerHTML === FULL_HEART) {
+        element.innerHTML = EMPTY_HEART
+        element.classList.remove("activated-heart")
+      }else {
+        element.innerHTML = FULL_HEART
+        element.classList = "activated-heart"
+      }
+
+    })
+  
+    .catch(error => {
+      const message = document.querySelector("#modal")
+      message.classList.remove("hidden")
+  
+      setTimeout( () => {
+        message.classList = "hidden"
+      }, 3000)
+    })
+  })
+
+})
 
 
 //------------------------------------------------------------------------------
